f4d685ea333f2cd6a6e803b357f8890dc11f6ab7
maurimartinezz <martinezmauriciojulian@gmail.com>
2022-08-06 00:49:06 -0300

    config eslint

--- src/services/board.service.ts
+++ src/services/board.service.ts
@@ -2,6 +2,6 @@ import { Board } from "../routes/types";
 import boardData from "./board.json";
 
 const boards: Array<Board> = boardData as Array<Board>;
-export const getEntries = () => boardData;
+export const getEntries = (): Array<Board> => boards;
 
 export const addEntry = () => null;
